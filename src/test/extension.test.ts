b66c8afb3af9fa07857f32b0061389f59282de61
Matt <matthew.therault@gmail>
2025-05-26 19:35:29 -0400

    fixed the package publisher

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,14 +3,13 @@ import * as vscode from 'vscode';
 
 suite('Sieve Language Support Test Suite', () => {
 	vscode.window.showInformationMessage('Start Sieve Language Support tests.');
-
 	test('Extension should be present', () => {
-		const extension = vscode.extensions.getExtension('undefined_publisher.sieve-language-support');
+		const extension = vscode.extensions.getExtension('your-publisher-name.sieve-language-support');
 		assert.ok(extension, 'Extension should be found');
 	});
 
 	test('Should activate extension', async () => {
-		const extension = vscode.extensions.getExtension('undefined_publisher.sieve-language-support');
+		const extension = vscode.extensions.getExtension('your-publisher-name.sieve-language-support');
 		if (extension) {
 			await extension.activate();
 			assert.ok(extension.isActive, 'Extension should be active');
